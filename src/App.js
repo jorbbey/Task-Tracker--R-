1ce4fc2330e393818f9b38d16db56e3735d7d0e2
Jorbbey <jorbbey@gmailcom>
2024-12-12 14:04:23 +0100

    server test

--- src/App.js
+++ src/App.js
@@ -1,120 +1,114 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
-import { useState, useEffect } from "react"
-import Header from "./components/Header"
-import Tasks from "./components/Tasks"
-import AddTask from "./components/AddTask"
-import Animations from "./components/Animations"
-import Footer from "./components/Footer"
-// const tasksData = 'http://localhost:5000/tasks'
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import Animations from "./components/Animations";
+import Footer from "./components/Footer";
+
 const App = () => {
-     
-     const [ addTaskTab, setAddTastTab ] = useState( false )
-     const [ tasks, setTasks ] = useState( [] )
+  const [addTaskTab, setAddTastTab] = useState(false);
+  const [tasks, setTasks] = useState([]);
 
-     const fetchTasks = async () => {                       // fetch tasks data
-          // const res = await fetch( tasksData )
-          const res = await fetch( `${ process.env.REACT_APP_API_URL}/tasks`)
-          const data = await res.json()
-          return data
-     }
+  // Fetch tasks
+ const fetchTasks = async () => {
+   const res = await fetch(`${process.env.REACT_APP_API_URL}/tasks`);
+   console.log("Response:", res);
+   const data = await res.json();
+   console.log("Data is: ", data);
+   setTasks(data);
+ };
 
-     fetchTasks()
 
-     // fetch task for toggle reminder
-     const fetchTask = async ( id ) => {                       // fetch tasks data
-          const res = await fetch( `${ process.env.REACT_APP_API_URL}/tasks }/${ id }` )
-          const data = await res.json()
-          // console.log( data );
-          return data
-     }
+  // Fetch task by id for reminder toggle
+  const fetchTask = async (id) => {
+    const res = await fetch(`${process.env.REACT_APP_API_URL}/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  };
 
-     useEffect( () => {
-          const getTasks = async () => {
-               const tasksFromServer = await fetchTasks()
-               setTasks( tasksFromServer )
-          }
-          getTasks()
-     }, [] )
+  // Fetch tasks when component mounts
+  useEffect(() => {
+    fetchTasks();
+  }, []);
 
-     // add task
-     const addTask = async ( task ) => {
-          const res = await fetch( `${process.env.REACT_APP_API_URL}/tasks}`, {
-               method: 'POST',
-               headers: {               // data is being added so content type needs to be specified
-                    'Content-type': 'application/json'
-               },
-               body: JSON.stringify( task )      // convert JS object to JSON string
-          } )
+  // Add task
+  const addTask = async (task) => {
+    const res = await fetch(`${process.env.REACT_APP_API_URL}/tasks`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
 
-          const data = await res.json()
-          setTasks( [ ...tasks, data ] )
+    const data = await res.json();
+    setTasks([...tasks, data]);
+  };
 
-          // const id = Math.floor( Math.random() * 10000 ) + 1
-          // const newTask = { id: id, ...task }
-          // setTasks( [ ...tasks, newTask ] )
-     }
+  // Delete task
+  const deleteTask = async (id) => {
+    await fetch(`${process.env.REACT_APP_API_URL}/tasks/${id}`, {
+      method: "DELETE",
+    });
 
-     // delete task
-     // DELETE request before filter on the UI - deletes the tasks permanently from the UI
-     const deleteTask = async ( id ) => {
-          await fetch( `${ process.env.REACT_APP_API_URL}/tasks }/${ id }`, {
-               method: 'DELETE'
-          } )
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
-          setTasks( tasks.filter( ( task ) => task.id !== id ) )
-          // console.log( 'delete', id );
-     }
+  // Toggle reminder
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = {
+      ...taskToToggle,
+      reminder: !taskToToggle.reminder,
+    };
 
-     // reminder toggle
-     const toggleReminder = async ( id ) => {
-          const taskToToggle = await fetchTask( id ) 
-          const updatedTask = {
-               ...taskToToggle,
-               reminder: !taskToToggle.reminder
-          }
+    const res = await fetch(`${process.env.REACT_APP_API_URL}/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updatedTask),
+    });
 
-          const res = await fetch( `${ process.env.REACT_APP_API_URL}/tasks }/${ id }`, {
-               method: 'PUT',
-               headers: {
-                    'Content-type': 'application/json'
-               },
-               body: JSON.stringify( updatedTask )
-          }
-          )
+    const data = await res.json();
 
-          const data = await res.json
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      )
+    );
+  };
 
-          setTasks(
-               tasks.map( ( task ) =>
-                    task.id === id ? { ...task, reminder: data.reminder } : task )
-          )
-     }
-     return (
-          <div className="w-full flex justify-center items-center bg-red-200">
-               <div className="w-[95%] md:w-[90%] bg-white h-auto my-32 rounded-xl">
-                    <Header title='Goal Guru'
-                         addTaskTab={ addTaskTab }
-                         onAdd={ () => setAddTastTab( !addTaskTab ) } />
-                    
-                    <div className="flex flex-col md:flex-row justify-around items-center">
-                         <div className="h-auto w-[95%] md:w-[40%]">
-                              { addTaskTab ? '' : <Animations /> }
-                              { addTaskTab && ( <AddTask onAdd={ addTask } /> ) }
-                         </div>
-                         { tasks.length > 0 ? (
-                              <Tasks tasks={ tasks }
-                                   onDelete={ deleteTask }
-                                   onToggle={ toggleReminder }
-                                   />
-                         ) : (
-                              <p className="text-gray-400 my-16 mx-16">Click the button above to add a task</p>
-                         ) }
-                    </div>
-                    <Footer />
-               </div>
+  return (
+    <div className="w-full flex justify-center items-center bg-red-200">
+      <div className="w-[95%] md:w-[90%] bg-white h-auto my-32 rounded-xl">
+        <Header
+          title="Goal Guru"
+          addTaskTab={addTaskTab}
+          onAdd={() => setAddTastTab(!addTaskTab)}
+        />
+        <div className="flex flex-col md:flex-row justify-around items-center">
+          <div className="h-auto w-[95%] md:w-[40%]">
+            {addTaskTab ? "" : <Animations />}
+            {addTaskTab && <AddTask onAdd={addTask} />}
           </div>
-          
-     )
-}
+          {tasks.length > 0 ? (
+            <Tasks
+              tasks={tasks}
+              onDelete={deleteTask}
+              onToggle={toggleReminder}
+            />
+          ) : (
+            <p className="text-gray-400 my-16 mx-16">
+              Click the button above to add a task
+            </p>
+          )}
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
